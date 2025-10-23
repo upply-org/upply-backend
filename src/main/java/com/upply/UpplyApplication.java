71d52c534a77a04f576930a87327febc4494b2b7
Mohamed-hanfy <mohamed.hanfy.dev@outlook.com>
2025-10-23 17:28:44 +0300

    add project structure

---
+++ src/main/java/com/upply/UpplyApplication.java
@@ -0,0 +1,13 @@
+package com.upply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upply/UpplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
