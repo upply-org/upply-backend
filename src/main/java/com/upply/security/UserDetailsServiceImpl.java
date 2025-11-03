874dab61fe9d458f40219e54e76e375e34b5c301
Kareem74x <hpg350@gmail.com>
2025-11-03 16:40:10 +0200

    fix(security): apply requested changes from PR review

--- src/main/java/com/upply/security/UserDetailsServiceImpl.java
+++ src/main/java/com/upply/security/UserDetailsServiceImpl.java
@@ -1,12 +1,13 @@
 package com.upply.security;
 
 import com.upply.user.UserRepository;
-import jakarta.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
