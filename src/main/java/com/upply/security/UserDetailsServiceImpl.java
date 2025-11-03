d8af7d8c3e4cf509714e4ba98941fb20086ebef3
Kareem74x <hpg350@gmail.com>
2025-11-03 16:33:29 +0200

    feat(security): implement user details service

--- src/main/java/com/upply/security/UserDetailsServiceImpl.java
+++ src/main/java/com/upply/security/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.upply.security;
 
+import com.upply.user.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,10 +12,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    // not implemented yet
+    private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+    // is important here because it ensures that when the user is loaded from the database, all related data
+    // like roles or authorities is fetched within the same open database session.
+    // Without it, if those relationships are lazy-loaded,
+    // you could get a LazyInitializationException later when Spring tries to access them outside the transaction.
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+
+        // pass email because it's the unique identifier of the user in the app
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with Email: " + userEmail));
+
     }
 }
